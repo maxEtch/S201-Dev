658da23078076016e8275c0d6c73ae055f47d518
Lucas Marrot <124559922+LucasMarrot@users.noreply.github.com>
2023-05-03 16:10:15 +0200

    Ajout de la v1 dans le GitHub
    v1 work in progress...

---
+++ lecteurDiapoV1/lecteurvue.cpp
@@ -0,0 +1,26 @@
+#include "lecteurvue.h"
+#include "ui_lecteurvue.h"
+
+LecteurVue::LecteurVue(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::LecteurVue)
+{
+    ui->setupUi(this);
+}
+
+LecteurVue::~LecteurVue()
+{
+    delete ui;
+}
+
+void LecteurVue::createStatusBar(bool estManuel)
+{
+    if (estManuel)
+    {
+        statusBar()->showMessage(tr("Statut : Manuel"));
+    }
+    else
+    {
+        statusBar()->showMessage(tr("Statut : Automatique"));
+    }
+}
