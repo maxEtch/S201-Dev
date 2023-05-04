026518d7b9bbe3d482878845d2f73aaa3504f0bc
Axel-Marrier <124685757+Axel-Marrier@users.noreply.github.com>
2023-05-04 15:51:27 +0200

    v1, version complète
    Dans le dernier commit, il manquait des éléments graphiques mais cet fois-ci ils sont tous là !

---
+++ lecteurvue.cpp
@@ -0,0 +1,110 @@
+#include "lecteurvue.h"
+#include "ui_lecteurvue.h"
+#include "QtWidgets"
+
+LecteurVue::LecteurVue(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::LecteurVue)
+{
+    ui->setupUi(this);
+    connect(ui->bSuivant,SIGNAL(clicked()),this, SLOT(avancer()));              //ok
+    connect(ui->bPrecedent,SIGNAL(clicked()),this, SLOT(reculer()));            //ok
+    connect(ui->bLancerDiapo,SIGNAL(clicked()),this,SLOT(lancer()));            //ok
+    connect(ui->bArreterDiapo,SIGNAL(clicked()),this,SLOT(arreter()));          //ok
+    connect(ui->actionCharger,SIGNAL(triggered()),this,SLOT(charger()));        //ok
+    connect(ui->actionVitesse,SIGNAL(triggered()),this,SLOT(modifVitesse()));   //ok
+    connect(ui->actionQuitter,SIGNAL(triggered()),this,SLOT(close()));          //ok
+    connect(ui->actionPropos,SIGNAL(triggered()),this,SLOT(propos()));          //ok
+
+    connect(ui->cCategorie1,SIGNAL(clicked(bool)),this,SLOT(categorie1(bool)));
+    connect(ui->cCategorie2,SIGNAL(clicked(bool)),this,SLOT(categorie2(bool)));
+    connect(ui->cCategorie3,SIGNAL(clicked(bool)),this,SLOT(categorie3(bool)));
+
+    creerBarStatus(true);
+}
+
+LecteurVue::~LecteurVue()
+{
+    delete ui;
+}
+
+void LecteurVue::creerBarStatus(bool estManuel)   //change l'état de la barre de status
+{
+    if (estManuel)
+    {
+        statusBar()->showMessage(tr("Statut : Manuel"));
+    }
+    else
+    {
+        statusBar()->showMessage(tr("Statut : Automatique"));
+    }
+}
+
+
+void LecteurVue::avancer() //marche
+{
+    qDebug() << "La flèche droite marche";
+
+}
+
+void LecteurVue::reculer() //marche
+{
+    qDebug() << "La flèche gauche marche";
+}
+
+void LecteurVue::lancer() //marche
+{
+    qDebug() << "Le bouton lancer marche";
+    ui->bArreterDiapo->setEnabled(true);    //active le bouton Arrèter diaporama
+    this->creerBarStatus(false);           //Change la bar de status pour afficher le mode automatique
+}
+
+void LecteurVue::arreter() //marche
+{
+    qDebug() << "Le bouton arreter marche";
+    ui->bArreterDiapo->setEnabled(false);   //Désactive le bouton Arreter le diaporama
+    this->creerBarStatus(true);            //Change l'état de la bar de status pour afficher le mode manuel
+}
+
+void LecteurVue::charger() //marche
+{
+    qDebug() << "Le bouton charger le diaporam marche";
+}
+
+void LecteurVue::modifVitesse() //marche
+{
+    qDebug() << "Le bouton modifier la vitesse marche";
+}
+
+void LecteurVue::propos()
+{
+    QMessageBox::information(this, "À propos de", "Va dans Paramètre pour charger un diaporama , puis explore les images avec les flèches ou appuies sur lancer"); //description provisoir
+     QDialog maDlg(nullptr);    //création de la boite de dialogue
+     maDlg.show();              //affiche la boite de dialogue
+
+     qDebug() << "Le bouton À propos de marche";
+}
+
+void LecteurVue::categorie1(bool etat)
+{
+    if(etat)
+    {
+        qDebug() << "La checkbox categorie 1 marche";
+    }
+}
+
+void LecteurVue::categorie2(bool etat)
+{
+    if(etat)
+    {
+        qDebug() << "La checkbox categorie 2 marche";
+    }
+}
+
+void LecteurVue::categorie3(bool etat)
+{
+    if(etat)
+    {
+        qDebug() << "La checkbox categorie 3 marche";
+    }
+}
---
+++ lecteurvue.h
@@ -0,0 +1,40 @@
+#ifndef LECTEURVUE_H
+#define LECTEURVUE_H
+
+#include <QMainWindow>
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class LecteurVue; }
+QT_END_NAMESPACE
+
+class LecteurVue : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    LecteurVue(QWidget *parent = nullptr);  //constructeur (variable à initialiser dedans si besoin)
+    ~LecteurVue();  //destructeur
+    void creerBarStatus(bool);  //permet de changer l'affichage dans la bar de status
+
+
+
+private:
+    Ui::LecteurVue *ui;
+
+private slots:
+    void avancer(); //fait défiler le diaporama vers l'image suivante
+    void reculer(); //fait défiler le diaporama vers l'image suivante
+    void lancer();  //change le mode en automatique
+    void charger(); //permet de sélectionner un diaporama à afficher
+    void modifVitesse();    //interface pour modifier la vitesse de défilement des images
+    void arreter(); //change le mode de défilement en manuel
+    void propos();  //affiche une boîte de dialogue pour aider l'utilisateur
+
+    void categorie1(bool);
+    void categorie2();
+    void categorie3();
+
+
+};
+#endif // LECTEURVUE_H
