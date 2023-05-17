bc42f155e70368278f8c8c0b0bf74328c4505bb2
Axel-Marrier <124685757+Axel-Marrier@users.noreply.github.com>
2023-05-17 08:53:02 +0200

    v2.1
    v2 fini !

--- lecteurvue.cpp
+++ lecteurvue.cpp
@@ -1,12 +1,14 @@
 #include "lecteurvue.h"
+#include "qmessagebox.h"
 #include "ui_lecteurvue.h"
-#include "QtWidgets"
 #include "image.h"
+#include "QString"
 
 LecteurVue::LecteurVue(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::LecteurVue)
 {
+
     ui->setupUi(this);
     connect(ui->bSuivant,SIGNAL(clicked()),this, SLOT(avancer()));              //ok
     connect(ui->bPrecedent,SIGNAL(clicked()),this, SLOT(reculer()));            //ok
@@ -16,13 +18,21 @@ LecteurVue::LecteurVue(QWidget *parent)
     connect(ui->actionVitesse,SIGNAL(triggered()),this,SLOT(modifVitesse()));   //ok
     connect(ui->actionQuitter,SIGNAL(triggered()),this,SLOT(close()));          //ok
     connect(ui->actionPropos,SIGNAL(triggered()),this,SLOT(propos()));          //ok
+    connect(ui->actionEnlever,SIGNAL(triggered()),this,SLOT(viderDiaporama())); //ok
 
     creerBarStatus(true);
+
+    //initialisation des variables
+    _numDiaporamaCourant = 0;
+    _posImageCourante = 0;
+
+
 }
 
 LecteurVue::~LecteurVue()
 {
     delete ui;
+    _diaporama.clear();
 }
 
 void LecteurVue::creerBarStatus(bool estManuel)   //change l'état de la barre de status
@@ -42,16 +52,25 @@ void LecteurVue::avancer() //marche
 {
     qDebug() << "La flèche droite marche";
     _posImageCourante++;
-    if (_posImageCourante > nbImages())
+    if (_posImageCourante == nbImages())
     {
-        _posImageCourante = 1;
+        _posImageCourante = 0;
     }
-
+    afficher();
 }
 
 void LecteurVue::reculer() //marche
 {
     qDebug() << "La flèche gauche marche";
+    if (_posImageCourante == 0)
+    {
+        _posImageCourante = nbImages()-1;
+    }
+    else
+    {
+        _posImageCourante--;
+    }
+    afficher();
 }
 
 void LecteurVue::lancer() //marche
@@ -70,34 +89,30 @@ void LecteurVue::arreter() //marche
 
 void LecteurVue::charger() //marche
 {
-    qDebug() << "Le bouton charger le diaporam marche";
+    qDebug() << "Le bouton charger le diaporama marche";
+
     Image* imageACharger;
 
+    viderDiaporama();
 
-    imageACharger = new Image(3, "personne", "Blanche Neige", "C:\\cartesDisney\\carteDisney2.gif");
+    imageACharger = new Image(3, "Personne", "Blanche Neige", ":/lecteurDiapoV2/Disney_4.gif");
     _diaporama.push_back(imageACharger);
-    imageACharger = new Image(2, "personne", "Cendrillon", "C:\\cartesDisney\\carteDisney4.gif");
+    imageACharger = new Image(2, "Personne", "Cendrillon", ":/lecteurDiapoV2/Disney_45.gif");
     _diaporama.push_back(imageACharger);
-    imageACharger = new Image(4, "animal", "Mickey", "C:\\cartesDisney\\carteDisney1.gif");
+    imageACharger = new Image(4, "Animal", "Mickey", ":/lecteurDiapoV2/Disney_19.gif");
     _diaporama.push_back(imageACharger);
-    imageACharger = new Image(1, "personne", "Grincheux", "C:\\cartesDisney\\carteDisney1.gif");
+    imageACharger = new Image(1, "Animal", "Bambi", ":/lecteurDiapoV2/Disney_3.gif");
     _diaporama.push_back(imageACharger);
 
-    bool estTrie = true;
-    while (estTrie)
-    {
-       estTrie = false;
-       for (int var = 0; var < static_cast<int>(_diaporama.size()); var++)
-       {
-           if (_diaporama[var]->getRang() > _diaporama[var+1]->getRang())
-           {
-               Image val = *_diaporama[var];
-               *_diaporama[var] = *_diaporama[var +1];
-               *_diaporama[var+1] = val;
-               estTrie = true;
-           }
-       }
-    }
+    triBulle(this->_diaporama);
+
+    ui->lImageMax->setNum(static_cast<int>(_diaporama.size()));
+
+    ui->bLancerDiapo->setDisabled(false);
+    ui->bSuivant->setDisabled(false);
+    ui->bPrecedent->setDisabled(false);
+
+    afficher();
 }
 
 void LecteurVue::modifVitesse() //marche
@@ -107,9 +122,105 @@ void LecteurVue::modifVitesse() //marche
 
 void LecteurVue::propos()
 {
-    QMessageBox::information(this, "À propos de", "Version 2.0, 5/05/2023, Maxime Etchecopar, Axel Marrier, Lucas Marrot"); //description provisoir
-     QDialog maDlg(nullptr);    //création de la boite de dialogue
-     maDlg.show();              //affiche la boite de dialogue
+    QMessageBox::information(this, "À propos de", "Version : Version 2.0\n Date création : 5/05/2023\n Auteurs : Maxime Etchecopar, Axel Marrier, Lucas Marrot"); //description provisoir
+    QDialog maDlg(nullptr);    //création de la boite de dialogue
+    maDlg.show();              //affiche la boite de dialogue
 
-     qDebug() << "Le bouton À propos de marche";
+    qDebug() << "Le bouton À propos de marche";
+}
+
+void LecteurVue::afficher()
+{
+
+    qDebug()<<"affichage démarre";
+
+    if (!_diaporama.empty())
+    {
+        qDebug() << "on rentre dans le if";
+
+        QString categorie = QString::fromStdString(imageCourante()->getCategorie());
+        ui->lCategorie->setText(categorie);
+
+        QString text = QString::fromStdString(imageCourante()->getTitre());
+        ui->lTitreImage->setText(text);
+
+        int nombre = imageCourante()->getRang();
+        ui->lImageActuel->setNum(nombre);
+
+        QString chemin = QString::fromStdString(imageCourante()->getChemin());
+        QImage cheminImage (chemin);
+        ui->image->setPixmap(QPixmap::fromImage(cheminImage));
+        ui->image->show();
+    }
+    else
+    {
+        ui->lCategorie->setText(tr("catégorie"));
+        ui->lTitreImage->setText(tr("titre"));
+        ui->lImageActuel->setText(tr("nbImageActuelle"));
+        ui->lImageMax->setText(tr("nbImageTotal"));
+        ui->image->setText(tr("image"));
+    }
+
+
+}
+
+unsigned int LecteurVue::nbImages()
+{
+    return _diaporama.size();
+}
+
+Image *LecteurVue::imageCourante()
+{
+    return _diaporama[_posImageCourante];
+}
+
+void LecteurVue::changerDiaporama(unsigned int pNumDiaporama)
+{
+    if (numDiaporamaCourant() > 0)
+    {
+        viderDiaporama();
+    }
+    _numDiaporamaCourant = pNumDiaporama;
+    if (numDiaporamaCourant() > 0)
+    {
+        charger();
+    }
+}
+
+unsigned int LecteurVue::numDiaporamaCourant()
+{
+    return _numDiaporamaCourant;
+}
+
+void LecteurVue::viderDiaporama()
+{
+    qDebug() << "le vidage se lance";
+    _diaporama.clear();
+    afficher();
+
+    _posImageCourante = 0;
+
+    ui->bLancerDiapo->setDisabled(true);
+    ui->bSuivant->setDisabled(true);
+    ui->bPrecedent->setDisabled(true);
+}
+
+
+void LecteurVue::triBulle(Diaporama &unDiaporama)
+{
+    bool estTrie = false;
+    while (!estTrie)
+    {
+       estTrie = true;
+       for (int var = 0; var < static_cast<int>(unDiaporama.size()-1); var++)
+       {
+           if (_diaporama[var]->getRang() > unDiaporama[var+1]->getRang())
+           {
+               Image val = *unDiaporama[var];
+               *unDiaporama[var] = *unDiaporama[var +1];
+               *unDiaporama[var+1] = val;
+               estTrie = false;
+           }
+        }
+     }
 }
